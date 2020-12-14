8ae7e1a1c946b18ce15cbceb786c98c1ef22cd92
nhdnc <arjay.otis@gmail.com>
2020-12-14 11:41:31 +0800

    Removed unnecessary console.log calls

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,5 @@
 
 
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-
-//     response.json().then((data) => {
-//         console.log(data)
-//     })
-
-// })
-
 /*fetch('http://localhost:3000/weather?address=boston').then((response) => {
     response.json().then((data) => {
         if(data.error){
